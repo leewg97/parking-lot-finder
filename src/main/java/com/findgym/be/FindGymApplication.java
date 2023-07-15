16a79fbfbb4c949ecb87800832e48a6f5ac59494
leewg97 <leewg97@naver.com>
2023-07-15 18:33:29 +0900

    gradle init

---
+++ src/main/java/com/findgym/be/FindGymApplication.java
@@ -0,0 +1,13 @@
+package com.findgym.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindGymApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/findgym/be/FindGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findgym.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindGymApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
