d366b671cc841bd075b5d40d6610e478c14bc405
leewg97 <leewg97@naver.com>
2023-07-18 02:12:12 +0900

    Feat #4 : Add DirectionService * 반경 10KM 내에 존재하는 헬스장을 찾기 위한 거리 계산 알고리즘 작성

--- src/main/java/com/findgym/be/direction/service/DirectionService.java
+++ src/main/java/com/findgym/be/direction/service/DirectionService.java
@@ -1,4 +1,65 @@
 package com.findgym.be.direction.service;
 
+import com.findgym.be.api.dto.DocumentDto;
+import com.findgym.be.direction.entity.Direction;
+import com.findgym.be.gym.service.GymSearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class DirectionService {
+
+    private static final int MAX_SEARCH_COUNT = 5; // 최대 겁색 갯수
+    private static final double RADIUS_KM = 10.0; // 반경 10km
+
+    private final GymSearchService gymSearchService;
+
+    public List<Direction> buildDirectionList(DocumentDto dto) {
+        if (Objects.isNull(dto)) return Collections.emptyList();
+
+        return gymSearchService.searchGymDtoList()
+                .stream()
+                .map(gymDto ->
+                        Direction.builder()
+                                .inputAddress(dto.getAddressName())
+                                .inputLatitude(dto.getLatitude())
+                                .inputLongitude(dto.getLongitude())
+                                .targetGymName(gymDto.getGymName())
+                                .targetAddress(gymDto.getGymAddress())
+                                .targetLatitude(gymDto.getLatitude())
+                                .targetLongitude(gymDto.getLongitude())
+                                .distance(
+                                        calculateDistance(
+                                                dto.getLatitude(), dto.getLongitude(),
+                                                gymDto.getLatitude(), gymDto.getLongitude()
+                                        )
+                                )
+                                .build()
+                )
+                .filter(direction -> direction.getDistance() <= RADIUS_KM)
+                .sorted(Comparator.comparing(Direction::getDistance))
+                .limit(MAX_SEARCH_COUNT)
+                .collect(Collectors.toList());
+    }
+
+    // HAVERSINE FORMULA
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+
+        double earthRadius = 6371; //Kilometers
+        return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    }
+
 }
