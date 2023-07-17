5f239c4b98015ab3af340101d6dbe58a0450e6d7
leewg97 <leewg97@naver.com>
2023-07-18 02:05:17 +0900

    Feat #4 : Add GymSearchService

---
+++ src/main/java/com/findgym/be/gym/service/GymSearchService.java
@@ -0,0 +1,39 @@
+package com.findgym.be.gym.service;
+
+import com.findgym.be.gym.dto.GymDto;
+import com.findgym.be.gym.entity.Gym;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GymSearchService {
+
+    private final GymRepositoryService gymRepositoryService;
+
+    public List<GymDto> searchGymDtoList() {
+        // TODO: redis
+
+        // db
+        return gymRepositoryService.findAll()
+                .stream()
+                .map(this::convertToGymDto)
+                .collect(Collectors.toList());
+    }
+
+    private GymDto convertToGymDto(Gym gym) {
+        return GymDto.builder()
+                .id(gym.getId())
+                .gymAddress(gym.getGymAddress())
+                .gymName(gym.getGymName())
+                .latitude(gym.getLatitude())
+                .longitude(gym.getLongitude())
+                .build();
+    }
+
+}
