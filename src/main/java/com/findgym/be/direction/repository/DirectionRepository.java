fc0700732a94fed9c691fd5919520f0a584756e6
leewg97 <leewg97@naver.com>
2023-07-18 01:59:21 +0900

    Feat #4 : Add Direction Repository

---
+++ src/main/java/com/findgym/be/direction/repository/DirectionRepository.java
@@ -0,0 +1,7 @@
+package com.findgym.be.direction.repository;
+
+import com.findgym.be.direction.entity.Direction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectionRepository extends JpaRepository<Direction, Long> {
+}
