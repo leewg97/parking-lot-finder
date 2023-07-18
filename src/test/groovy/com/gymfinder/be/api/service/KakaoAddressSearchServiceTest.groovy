743855d93f3b79c05235497a87a896648cec7652
leewg97 <leewg97@naver.com>
2023-07-19 01:00:27 +0900

    Test #6 : 정상적인 주소를 입력헀을 경우, 정상적으로 위도 경도를 반환하는 테스트 작성

--- src/test/groovy/com/gymfinder/be/api/service/KakaoAddressSearchServiceTest.groovy
+++ src/test/groovy/com/gymfinder/be/api/service/KakaoAddressSearchServiceTest.groovy
@@ -8,7 +8,7 @@ class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest
     @Autowired
     private KakaoAddressSearchService kakaoAddressSearchService
 
-    def "address 파라미터 값이 null이면, requestAddressSearch 메소드는 null을 리턴한다."() {
+    def "주소 파라미터 값이 null 이면, requestAddressSearch 메소드는 null 을 리턴한다."() {
         given:
         String address = null
 
@@ -19,7 +19,7 @@ class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest
         result == null
     }
 
-    def "주소값이 valid하다면, requestAddressSearch 메소드는 정상적으로 document를 반환한다."() {
+    def "주소값이 유효하다면, requestAddressSearch 메소드는 정상적으로 document 를 반환한다."() {
         given:
         def address = "서울 서초구 사평대로 371"
 
@@ -30,6 +30,28 @@ class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest
         result.documentList.size() > 0
         result.metaDto.totalCount > 0
         result.documentList.get(0).addressName != null
+    }
+
+    def "정상적인 주소를 입력했을 경우, 정상적인 위도 경도로 반환된다."() {
+        given:
+        boolean actualResult = false
+
+        when:
+        def searchResult = kakaoAddressSearchService.requestAddressSearch(inputAddress)
+
+        then:
+        if (searchResult == null) actualResult = false
+        else actualResult = searchResult.getDocumentList().size() > 0
+
+        where:
+        inputAddress         | expectedResult
+        "서울 서초구 사평대로"        | true
+        "서울 강남구 논현동 91"      | true
+        "서울 대학로"             | true
+        "서울 강남구 잘못된 주소"      | false
+        "송파구 송파동 243"        | true
+        "송파구 잠실동 243-243333" | false
+        ""                   | false
     }
 
 }
