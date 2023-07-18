7e25abde5a456c6a97f2b2d20f36b6eb3e4cb81a
leewg97 <leewg97@naver.com>
2023-07-19 00:59:28 +0900

    Fix : 순환참조 문제 해결 * GymSearchService 와 DirectionService 의 순환참조 문제 해결

---
+++ src/main/java/com/gymfinder/be/gym/service/GymFindingService.java
@@ -0,0 +1,36 @@
+package com.gymfinder.be.gym.service;
+
+import com.gymfinder.be.api.dto.DocumentDto;
+import com.gymfinder.be.api.dto.KakaoApiResponseDto;
+import com.gymfinder.be.api.service.KakaoAddressSearchService;
+import com.gymfinder.be.direction.entity.Direction;
+import com.gymfinder.be.direction.service.DirectionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GymFindingService {
+
+    private final DirectionService directionService;
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+
+    public void searchGymList(String address) {
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+
+        if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
+            log.error("[GymSearchService searchGymList fail] Input address : {}", address);
+            return;
+        }
+
+        DocumentDto dto = kakaoApiResponseDto.getDocumentList().get(0);
+        List<Direction> directionList = directionService.buildDirectionList(dto);
+        directionService.saveAll(directionList);
+    }
+}
--- src/main/java/com/gymfinder/be/gym/service/GymSearchService.java
+++ src/main/java/com/gymfinder/be/gym/service/GymSearchService.java
@@ -1,19 +1,12 @@
 package com.gymfinder.be.gym.service;
 
-import com.gymfinder.be.api.dto.DocumentDto;
-import com.gymfinder.be.api.dto.KakaoApiResponseDto;
-import com.gymfinder.be.api.service.KakaoAddressSearchService;
-import com.gymfinder.be.direction.entity.Direction;
-import com.gymfinder.be.direction.service.DirectionService;
 import com.gymfinder.be.gym.dto.GymDto;
 import com.gymfinder.be.gym.entity.Gym;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -21,22 +14,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class GymSearchService {
 
-    private final DirectionService directionService;
     private final GymRepositoryService gymRepositoryService;
-    private final KakaoAddressSearchService kakaoAddressSearchService;
-
-    public void searchGymList(String address) {
-        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
-
-        if (Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
-            log.error("[GymSearchService searchGymList fail] Input address : {}", address);
-            return;
-        }
-
-        DocumentDto dto = kakaoApiResponseDto.getDocumentList().get(0);
-        List<Direction> directionList = directionService.buildDirectionList(dto);
-        directionService.saveAll(directionList);
-    }
 
     public List<GymDto> searchGymDtoList() {
         // TODO: redis
